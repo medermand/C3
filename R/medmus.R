b62f741a812522d3a6b39669cf34ff04a89a4362
medermand <ku.meder@gmail.com>
2016-07-26 15:57:16 +0300

    file uploaded

--- R/medmus.R
+++ R/medmus.R
@@ -1,30 +1,26 @@
-rm(list=ls())
-library("htmlwidgets")
-library("devtools")
 
-rjs<-function(arg){
-  
+
+medmus<-function(arg){
+
   a<-strsplit(arg, "-")
-  
+
   load("file")
-  
+
   ind.seg=a[[1]][4] #" Software"
   gra=as.numeric(a[[1]][3]) #14
   job.fam=a[[1]][1] # #"Human Resources (HR)"
   job.sfam=a[[1]][2] #"HR Generalist (HRA)"
   salary=as.numeric(a[[1]][5])
-  
-  
+
+
   q1<-which(file$GRADE==gra)
   q2<-which(file$Industry.Segment==ind.seg)
   q3<-which(file$SHORT.CODE==job.fam)
   q4<-which(file$JOB.FAMILY.CODE==job.sfam)
-  
+
   qe<-intersect(intersect(q1,q2),intersect(q3,q4))
-  
+
   return(file$salary[qe])
 }
 
-getName<-function(name){
-  return(name)
-}
+
---
+++ test.R
@@ -0,0 +1,6 @@
+#I am learning a new language whihc is called an R language
+meder <- function(a){
+  print(a*a)
+}
+
+meder(5)
